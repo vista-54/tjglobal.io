0c8c0a4307cb51a068be87c2e1beee3159134d81
kravcovvalentin@gmail.com <kravcovvalentin@gmail.com>
2018-03-27 16:39:36 +0300

    27.03.18-4

--- js/effects.js
+++ js/effects.js
@@ -1,16 +1,33 @@
-$(document).ready(function(){
-    $("#anchor").on("click","a", function (event) {
+
+
+$(document).ready(function () {
+    $("#anchor").on("click", "a", function (event) {
 
         event.preventDefault();
-        var id  = $(this).attr('href'),
+        var id = $(this).attr('href'),
             top = $(id).offset().top;
         // console.log(id);
-        if(id == '#company_spreedsheet'){
-            top = top-200;
+        if (id == '#company_spreedsheet') {
+            top = top - 200;
             // return top;
             $('body,html').animate({scrollTop: top}, 1000);
         }
 
         $('body,html').animate({scrollTop: top}, 1000);
     });
+});
+
+
+$(window).scroll(function () {
+
+    var wScroll = $(this).scrollTop();
+
+    if (wScroll > $('#last_section').offset().top) {
+
+        $('#last_section').addClass('fourth');
+
+    }
+    else if (wScroll > $('#last_section').offset().top-100){
+        $('#last_section').removeClass('fourth');
+    }
 });
